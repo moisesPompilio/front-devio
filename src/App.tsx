0f702e3ec9493211ac79e76deeeda52e45a7b899
moisesPompilio <moisesjtmed@gmail.com>
2023-05-27 00:41:36 -0300

    feat: reduce configuration, in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.scss';
-import Home from './pages/Home/Home';
+import { Provider } from 'react-redux';
+import { Home } from './pages/Home/Home';
+import { store } from './store/store';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     </div>
   );
 }
