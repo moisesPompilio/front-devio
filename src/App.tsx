7c7f269a59cf4f24b933f4d361a10225895f6c7e
moisesPompilio <moisesjtmed@gmail.com>
2023-05-28 23:16:17 -0300

    feat: placement of the navBar, page cart and page requests, in the app.tsx so they can be displayed

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.scss';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { Home } from './pages/Home/Home';
 import { store } from './store/store';
+import { Footer } from './components/Footer/Footer';
+import { NavBar } from './components/NavBar/NavBar';
+import { Pedidos } from './pages/Pedidos/Pedidos';
+import { Cart } from './pages/Cart/Cart';
 
 function App() {
   return (
     <div className="App">
       <Provider store={store}>
         <BrowserRouter>
+          <NavBar />
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/pedidos" element={<Pedidos />} />
           </Routes>
+          <Footer />
         </BrowserRouter>
       </Provider>
     </div>
